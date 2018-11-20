551de62baebe87a847ed02669bf33a983563a191
Your Name <lauraelgozi@gmail.com>
2018-11-20 12:03:49 +0200

    ass6

--- js/minecraft.js
+++ js/minecraft.js
@@ -1,34 +1,65 @@
-$(document).ready(function (){
+$(document).ready(function () {
+    var Minecraft = {
+        boardArr: [
+            ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky"],
+            ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky"],
+            ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky"],
+            ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky"],
+            ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky"],
+            ["sky", "sky", "sky", "sky", "sky", "white", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky"],
+            ["sky", "sky", "sky", "white", "white", "white", "white", "sky", "white", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky"],
+            ["sky", "sky", "white", "white", "white", "white", "white", "white", "white", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky"],
+            ["sky", "sky", "sky", "sky", "sky", "sky", "white", "white", "sky", "sky", "sky", "sky", "sky", "sky", "green", "green", "green", "sky", "sky", "sky"],
+            ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "green", "green", "green", "sky", "sky", "sky"],
+            ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "green", "green", "green", "sky", "sky", "sky"],
+            ["sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "trunk", "sky", "sky", "sky", "sky"],
+            ["sky", "sky", "sky", "sky", "green", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "sky", "trunk", "sky", "sky", "sky", "sky"],
+            ["sky", "sky", "sky", "green", "green", "green", "sky", "sky", "sky", "sky", "sky", "sky", "rock", "rock", "sky", "trunk", "sky", "sky", "sky", "rock"],
+            ["pierre", "pierre", "pierre", "pierre", "pierre", "pierre", "pierre", "pierre", "pierre", "pierre", "pierre", "pierre", "pierre", "pierre", "pierre", "pierre", "pierre", "pierre", "pierre", "pierre"],
+            ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt"],
+            ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt"],
+            ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt"],
+            ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt"],
+            ["dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt", "dirt"],
+        ]
+    };
+    /**
+     * ************************* Create html using js Section ***********************************
+     */
+    var menu = $("<div/>");
+    menu.addClass("menu");
+    $("body").append(menu);
+    var pickaxe = $("<div/>");
+    pickaxe.addClass("pickaxe");
+    menu.append(pickaxe);
+    var shovel = $("<div/>");
+    shovel.addClass("shovel");
+    menu.append(shovel);
+    var axe = $("<div/>");
+    axe.addClass("axe");
+    menu.append(axe);
+    var board = $("<div/>");
+    board.addClass("board");
+    $("body").append(board);
 
-var boardArr =[
-    ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky"],
-    ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky"],
-    ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky"],
-    ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky"],
-    ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky"],
-    ["sky","sky","sky","sky","sky","white","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky"],
-    ["sky","sky","sky","white","white","white","white","sky","white","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky"],
-    ["sky","sky","white","white","white","white","white","white","white","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky"],
-    ["sky","sky","sky","sky","sky","sky","white","white","sky","sky","sky","sky","sky","sky","green","green","green","sky","sky","sky"],
-    ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","green","green","green","sky","sky","sky"],
-    ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","green","green","green","sky","sky","sky"],
-    ["sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","trunk","sky","sky","sky","sky"],
-    ["sky","sky","sky","sky","green","sky","sky","sky","sky","sky","sky","sky","sky","sky","sky","trunk","sky","sky","sky","sky"],
-    ["sky","sky","sky","green","green","green","sky","sky","sky","sky","sky","sky","rock","rock","sky","trunk","sky","sky","sky","rock"],
-    ["pierre","pierre","pierre","pierre","pierre","pierre","pierre","pierre","pierre","pierre","pierre","pierre","pierre","pierre","pierre","pierre","pierre","pierre","pierre","pierre"],
-    ["dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt"],
-    ["dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt"],
-    ["dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt"],
-    ["dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt"],
-    ["dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt","dirt"],
-];
-var board = $("#board");
-for (var i = 0; i < boardArr.length; i++){
-    for(var j=0; j < boardArr[i].length; j++){
-        var pixel =$("<div/>");
-        pixel.addClass(boardArr[i][j]);
-        board.append(pixel);
+    Minecraft.createBoard = function () {
+        for (var i = 0; i < Minecraft.boardArr.length; i++) {
+            for (var j = 0; j < Minecraft.boardArr[i].length; j++) {
+                var pixel = $("<div/>");
+                pixel.addClass(Minecraft.boardArr[i][j]);
+                board.append(pixel);
 
+            }
+        }
     }
-}
+    Minecraft.createBoard();
+    newButton.addEventListenner("click", function (e) {
+        if (Minecraft.selectedMenu !== undefined) {
+            $("#Minecraft.selectedMenu").removeClass("selected");
+        }
+            var clickedMenu = e.target;
+            Minecraft.selectedMenu = clickedMenu.id;
+            clickedMenu.addClass("selected");
+        });
+
 });
